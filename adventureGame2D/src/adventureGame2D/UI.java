388b97da9faaace61570dc2f86e8b5cb96cb94f4
bollabollo <bollabollo@v1041-wn-rt-a-137-201.campus-dynamic.uwaterloo.ca>
2023-01-26 19:33:02 -0500

    add end game screen and playtime display

--- adventureGame2D/src/adventureGame2D/UI.java
+++ adventureGame2D/src/adventureGame2D/UI.java
@@ -4,23 +4,29 @@ import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
+import java.text.DecimalFormat;
 
 import object.Obj_key;
 
 public class UI {
 	
 	GamePanel gp;
-	Font arial_30;
+	Font arial_30, arial_50, arial_70;
 	BufferedImage keyImage;
 	public boolean messageOn = false;
 	public String message ="";
 	private int messageCount=0;
 	public boolean gameCompleted = false;
 	
+	double playTime;
+	DecimalFormat dFormat = new DecimalFormat("#0.00");
+	
 	public UI(GamePanel gp) {
 		this.gp = gp;
 		
 		arial_30 = new Font ("Arial", Font.PLAIN, 30);
+		arial_50 = new Font ("Arial", Font.PLAIN, 50);
+		arial_70 = new Font ("Arial", Font.BOLD, 70);
 		Obj_key Key = new Obj_key();
 		keyImage = Key.image;
 	}
@@ -35,19 +41,38 @@ public class UI {
 	//Draws the UI
 	public void draw(Graphics2D g2) {
 		if (gameCompleted == true) {
-			g2.setFont(arial_30);
+			g2.setFont(arial_50);
 			g2.setColor(Color.white);
 			String text;
 			int x, y, textLength;
 			
-			text ="Congratulations";
-			
-			text = "You finished the game";
+			//Draws the end game scene
+			text = "You found the ultimate treasure";
 			textLength = (int)g2.getFontMetrics().getStringBounds(text, g2).getWidth();
-			
 			x = gp.screenWidth/2 - textLength/2;
-			y = gp.screenHeight/2 - (gp.tileSize*3);
+			y = gp.screenHeight/2 - (gp.tileSize);
 			g2.drawString(text, x, y);
+			
+			text = "Your total playtime is: " + dFormat.format(playTime)
++ "!";			
+			textLength = (int)g2.getFontMetrics().getStringBounds(text, g2).getWidth();
+			x = gp.screenWidth/2 - textLength/2;
+			y = gp.screenHeight/2 + (gp.tileSize*4);
+			g2.drawString(text, x, y);
+			
+			
+			g2.setFont(arial_70);
+			g2.setColor(Color.YELLOW);
+			text ="Congratulations";
+			textLength = (int)g2.getFontMetrics().getStringBounds(text, g2).getWidth();
+			x = gp.screenWidth/2 - textLength/2;
+			y = gp.screenHeight/2 + (gp.tileSize*2);
+			g2.drawString(text, x, y);
+			
+			
+			
+			//stops the thread and the game
+			gp.gameThread = null;
 			
 			
 			
@@ -58,6 +83,10 @@ public class UI {
 			g2.drawImage(keyImage, gp.tileSize/2, gp.tileSize/2, gp.tileSize, gp.tileSize, null);
 			//drawString indicates the baseline of the text - drawString and PaintComponents x y values are different 
 			g2.drawString("x " + gp.player.hasKey, 74, 65);
+			
+			//Tracks time
+			playTime += (double)1/60;
+			g2.drawString("Time: " + dFormat.format(playTime), gp.tileSize*13, 65);
 			
 			//MESSAGE
 			if (messageOn == true) {
