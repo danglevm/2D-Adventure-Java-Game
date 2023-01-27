0cc779acb33d7ed906470823a535841c7b99c01d
bollabollo <bollabollo@v1041-wn-rt-a-137-201.campus-dynamic.uwaterloo.ca>
2023-01-26 20:30:55 -0500

    create UtilityTool.java to store useful fns

---
+++ adventureGame2D/src/adventureGame2D/UtilityTool.java
@@ -0,0 +1,27 @@
+package adventureGame2D;
+
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+
+public class UtilityTool {
+	//put useful functions in here
+	
+	
+	
+	//scale tile images
+	public BufferedImage scaleImage (BufferedImage original, int width, int height) {
+		//Scale the image before being drawn - pass width, height, image type
+		BufferedImage scaledImage = new BufferedImage(width, height, original.getType());
+		
+		//Whatever g2 is going to draw it is going be stored into scaledImage
+		Graphics2D g2 = scaledImage.createGraphics();
+		
+		//draw tile.image into scaledImage that the graphics 2d is linked to
+		g2.drawImage(original, 0, 0, width, height, null);
+		
+		g2.dispose();
+		//reset the tile into the scaled Image
+		return scaledImage;
+	}
+	
+}
