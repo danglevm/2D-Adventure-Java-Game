8161f4b6a67a4a33b97f0b3388dfb950eb2e32d8
bollabollo <bollabollo@desktop>
2023-01-28 13:17:11 -0500

    temporary remove objects placement

--- adventureGame2D/src/adventureGame2D/AssetPlacement.java
+++ adventureGame2D/src/adventureGame2D/AssetPlacement.java
@@ -15,22 +15,7 @@ public class AssetPlacement {
 	
 	public void setObject() {
 		
-		gp.obj[0] = new Obj_key(gp);
-		gp.obj[0].worldX = 115 *gp.tileSize;
-		gp.obj[0].worldY = 145*gp.tileSize;
-		
-		
-		gp.obj[2] = new Obj_door(gp);
-		gp.obj[2].worldX = 121*gp.tileSize;
-		gp.obj[2].worldY = 123 *gp.tileSize;
-		
-		gp.obj[3] = new Obj_chest(gp);
-		gp.obj[3].worldX = 121*gp.tileSize;
-		gp.obj[3].worldY = 122 *gp.tileSize;
-		
-		gp.obj[4] = new Obj_boots(gp);
-		gp.obj[4].worldX = 137*gp.tileSize;
-		gp.obj[4].worldY = 122*gp.tileSize;
-	}
 	
+	
+}
 }
