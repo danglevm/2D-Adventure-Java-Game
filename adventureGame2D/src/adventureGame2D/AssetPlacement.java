d4bd76da6b4095a1d4e25b4bf94ec2603075b6b7
bollabollo <bollabollo@v1041-wn-rt-a-236-215.campus-dynamic.uwaterloo.ca>
2023-01-29 20:18:35 -0500

    place Old Dude NPC on the map

--- adventureGame2D/src/adventureGame2D/AssetPlacement.java
+++ adventureGame2D/src/adventureGame2D/AssetPlacement.java
@@ -1,5 +1,6 @@
 package adventureGame2D;
 
+import entity.NPC_OldDude;
 import object.Obj_boots;
 import object.Obj_chest;
 import object.Obj_door;
@@ -17,5 +18,11 @@ public class AssetPlacement {
 		
 	
 	
-}
+	}
+	
+	public void setNPCs() {
+		gp.npcs[0] = new NPC_OldDude(gp);
+		gp.npcs[0].WorldX = 112*gp.tileSize;
+		gp.npcs[0].WorldY = 135*gp.tileSize;
+	}
 }
