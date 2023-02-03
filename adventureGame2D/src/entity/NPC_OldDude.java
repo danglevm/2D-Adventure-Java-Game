0c7cdb9008e1deab84ca3810f9373ea8b5380a78
bollabollo <bollabollo@v1041-wn-rt-a-236-215.campus-dynamic.uwaterloo.ca>
2023-02-02 22:13:26 -0500

    more family friend voicelines

--- adventureGame2D/src/entity/NPC_OldDude.java
+++ adventureGame2D/src/entity/NPC_OldDude.java
@@ -48,7 +48,7 @@ public class NPC_OldDude extends Entity {
 		//Meeting the player for the first time
 		oldDudeDialogues[0] = " '...' ";
 		oldDudeDialogues[1] = " 'OH what the-' ";
-		oldDudeDialogues[2] = " 'Who the f*** are you, whippersnapper?\n Creeping on me like that?' ";
+		oldDudeDialogues[2] = " 'Who the heck are you, whippersnapper?\n Creeping on me like that?' ";
 		
 		//depends on what the player chooses
 		oldDudeDialogues[3] = " 'You deaf, son?' ";
