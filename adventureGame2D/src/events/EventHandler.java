b3fea0f460ab8671e9b0b06a7f7d1e3e3607e55a
bollabollo <bollabollo@desktop>
2023-02-05 22:18:48 -0500

    initialize every tile and make event happen only once

--- adventureGame2D/src/events/EventHandler.java
+++ adventureGame2D/src/events/EventHandler.java
@@ -1,50 +1,76 @@
-package adventureGame2D;
+package events;
 
 import java.awt.Rectangle;
+
+import adventureGame2D.GamePanel;
 
 public class EventHandler {
 	
 	
 	GamePanel gp;
 	Event eventObject;
-	Rectangle eventRectangle;
-	protected int eventRectangleDefaultX, eventRectangleDefaultY;
+	EventRectangle eventRect[][];
 	
 	public EventHandler (GamePanel gp) {
 		this.gp = gp;
-		eventObject = new Event (gp);
-		//x, y, width, height
-		eventRectangle = new Rectangle(23, 23, 2, 2);
-		eventRectangleDefaultX = eventRectangle.x;
-		eventRectangleDefaultY = eventRectangle.y;
+		int col = 0, row = 0;
+		eventRect = new EventRectangle [gp.maxWorldCol][gp.maxWorldRow];
+		
+		while (col < gp.maxWorldCol && row < gp.maxWorldRow) {
+			
+			//Initializes every tile to be a potential event tile
+			
+			eventObject = new Event (gp);
+			//x, y, width, height
+			eventRect[col][row] = new EventRectangle();
+			eventRect[col][row].x = 23;
+			eventRect[col][row].y = 23;
+			eventRect[col][row].width = 23;
+			eventRect[col][row].height = 23;
+			eventRect[col][row].eventDefaultX = eventRect[col][row].x;
+			eventRect[col][row].eventDefaultY = eventRect[col][row].y;
+			
+			++col;
+			if (col == gp.maxWorldCol) {
+				col = 0;
+				row ++;
+			}
+		}
 		
 	}
 	
 
 	
 	public void checkEvent() {
+		int pitX = 121, pitY = 122; 
+		int healX = 115, healY = 121;
 		
 		//damage player
-		if (eventCollision(121,122, "any")) {
+		if (eventCollision(pitX, pitY, "any")) {
 			eventObject.DamagePit(gp.dialogueState);
+			eventRect[pitX][pitY].eventTriggered = true;
 		}
 		
 		//heal player
-		if (eventCollision(115,121, "any")) {
+		if (eventCollision(healX, healY, "any")) {
 			eventObject.healingPool(gp.dialogueState);
+			eventRect[healX][healY].eventTriggered = true;
 		}
 	}
 	
-	public boolean eventCollision (int eventCol, int eventRow, String reqDirection)
+	public boolean eventCollision (int col, int row, String reqDirection)
 		{
 			boolean playerCollision = false;
 			
+			//get location of player and event rectangle solid area
 			gp.player.solidArea.x = gp.player.WorldX + gp.player.solidArea.x;
 			gp.player.solidArea.y = gp.player.WorldY + gp.player.solidArea.y;
-			eventRectangle.x = eventCol * gp.tileSize + eventRectangle.x;
-			eventRectangle.y = eventRow * gp.tileSize + eventRectangle.y;
+			eventRect[col][row].x = col * gp.tileSize + eventRect[col][row].x;
+			eventRect [col][row].y = row * gp.tileSize + eventRect[col][row].y;
 			
-			if (gp.player.solidArea.intersects(eventRectangle)) {
+			
+			//if player hits the event and the event has not been triggered
+			if (gp.player.solidArea.intersects(eventRect[col][row]) && !eventRect[col][row].eventTriggered) {
 				if (gp.player.direction.contentEquals(reqDirection) || reqDirection.contentEquals("any")) {
 					playerCollision = true;
 				}
@@ -52,9 +78,9 @@ public class EventHandler {
 			
 			gp.player.solidArea.x = gp.player.solidAreaDefaultX;
 			gp.player.solidArea.y = gp.player.solidAreaDefaultY;
-			eventRectangle.x = eventRectangleDefaultX;
-			eventRectangle.y = eventRectangleDefaultY;
-			
+			eventRect[col][row].x = eventRect[col][row].eventDefaultX;
+			eventRect[col][row].y = eventRect[col][row].eventDefaultY;
+		
 			return playerCollision;
 		}
 
