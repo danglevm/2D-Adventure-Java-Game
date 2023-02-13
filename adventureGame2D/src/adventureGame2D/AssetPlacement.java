10d6178a15816d6727a88da14bf8cc2a8fe93b9e
Bollabollo <danglevuminh@gmail.com>
2023-02-12 21:35:19 -0500

    accomodate changes made in constructor parameter values

--- adventureGame2D/src/adventureGame2D/AssetPlacement.java
+++ adventureGame2D/src/adventureGame2D/AssetPlacement.java
@@ -1,6 +1,7 @@
 package adventureGame2D;
 
 import entity.NPC_OldDude;
+import monster.Monster_Green_Slime;
 import object.Obj_boots;
 import object.Obj_chest;
 import object.Obj_door;
@@ -15,20 +16,17 @@ public class AssetPlacement {
 	}
 	
 	public void setObject() {
-		
-		gp.obj[0] = new Obj_door(gp);
-		gp.obj[0].WorldX = gp.tileSize*121;
-		gp.obj[0].WorldY = gp.tileSize*123;
-	
-		
-	
-	
-	
+		//121, 123
+		gp.objects.add(new Obj_key(gp, 121, 123));
 	}
 	
 	public void setNPCs() {
-		gp.npcs[0] = new NPC_OldDude(gp);
-		gp.npcs[0].WorldX = 112*gp.tileSize;
-		gp.npcs[0].WorldY = 135*gp.tileSize;
+		gp.NPCs.add(new NPC_OldDude(gp, 121, 136));
 	}
+	
+	public void setMonsters() {
+		gp.monsters.add(new Monster_Green_Slime (gp, 126, 136));
+		gp.monsters.add(new Monster_Green_Slime (gp, 110, 120));
+	}
+	
 }
