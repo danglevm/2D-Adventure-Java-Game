d4786810898a93622da735dd19310995f9522637
Bollabollo <danglevuminh@gmail.com>
2023-02-14 19:15:18 -0500

    some commenting

--- adventureGame2D/src/monster/Monster_Interface.java
+++ adventureGame2D/src/monster/Monster_Interface.java
@@ -1,12 +1,17 @@
 package monster;
 
-public class Monster_Interface {
-	public static interface Monster{
-		
+import entity.Entity;
+
+interface Monster_Interface{
 		//set default values for the entity
 		//an abstract method does not have a body
 		//classes that implements the interface, the methods are automatically public
 		abstract void setDefaultValues();
 		abstract void getImage();
-	}
+		//for overriding by monsters
+
+	
 }
+
+
+
