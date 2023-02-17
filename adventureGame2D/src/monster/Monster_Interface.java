551df09d9a4496aa31645ab6162225390e296b6f
Bollabollo <danglevuminh@gmail.com>
2023-02-16 21:35:38 -0500

    cleanup

--- adventureGame2D/src/monster/Monster_Interface.java
+++ adventureGame2D/src/monster/Monster_Interface.java
@@ -7,7 +7,7 @@ interface Monster_Interface{
 		//an abstract method does not have a body
 		//classes that implements the interface, the methods are automatically public
 		abstract void setDefaultValues();
-		abstract void getImage();
+		abstract void getImage(int size);
 		//for overriding by monsters
 
 	
