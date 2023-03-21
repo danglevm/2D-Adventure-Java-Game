b87bdb04b7a792f3726932550fc5f58ea7655c23
Bollabollo <danglevuminh@gmail.com>
2023-03-20 22:31:56 -0400

    add hashmap to track upgrades and desc changes
    - new linked hash map added to update player upgrades
    - update UI to reflect changes
    - change some item descriptions

--- adventureGame2D/src/object/interfaces/ObjectInterface.java
+++
@@ -1,9 +0,0 @@
-package object.interfaces;
-
-//automatically public methods but I want to limit the visibility so I create abstract methods in the inherited objects instead
-
-public interface ObjectInterface {
-	
-	abstract void setDefaultAttributes();
-	
-}
